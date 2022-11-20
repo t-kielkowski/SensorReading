6b52d57da35107f224d2a2f03d50c74eda20c9fe
Tomasz Kielkowski <71585480+t-kielkowski@users.noreply.github.com>
2022-11-20 13:58:13 +0100

    Working version - client

--- Client.cpp
+++ Client.cpp
@@ -2,23 +2,25 @@
 #include <WiFiClient.h>
 #include <ESPAsyncTCP.h>
 #include <ESPAsyncWebServer.h>
-#include "ClosedCube_SHT31D.h"
 #include "ClosedCube_HDC1080.h"
 
-ClosedCube_SHT31D sht3xd;
 ClosedCube_HDC1080 hdc1080;
 AsyncWebServer server(80);
 
 const char* ssid = "UL_1";
 const char* password = "123456789";
-const char* sleepTimeArg = "deepSleep";
+const char* sleepTimeArg = "sleepTime";
+const float maxVoltage = 3.30f;
+const float minVoltage = 2.45f;
+const float factor = maxVoltage - minVoltage; 
 bool shouldDeepSleep = false;
-String sleepTime;
 long long deepSleepTime;
 char* p;
 
-float temperature = 0.0;
-float humidity = 0.0;
+float weight = 0.0;
+float temp = 0.0;
+float hum = 0.0;
+int battLev = 0;
 
 void setup(){	
   Serial.begin(115200); 
@@ -36,12 +38,11 @@ void setup(){
 
 void InitializeSensor(){
     hdc1080.begin(0x40);
-    sht3xd.begin(0x44);
 }
 
 void loop(){
 	
-	UpdateSensors(sht3xd.readTempAndHumidity(SHT3XD_REPEATABILITY_HIGH, SHT3XD_MODE_POLLING, 50));
+	UpdateSensors();
 
 	if(shouldDeepSleep)
 	{
@@ -52,31 +53,40 @@ void loop(){
 	delay(5000); 	
 }
 
-void UpdateSensors(SHT31D result){
-    t = result.t;
-    h = result.rh;
+void UpdateSensors(){
+    temp = hdc1080.readTemperature();
+	Serial.print("Temperature: ");
+	Serial.println(temp);
+    hum = hdc1080.readHumidity();
+	Serial.print("Hum: ");
+	Serial.println(hum);
+	battLev = GetBaterryLevel();
 }
 
 void SetServerMethod(){
 	
+	server.on("/weight", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/plain", String(weight).c_str());
+	});
+	
 	server.on("/temperature", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send_P(200, "text/plain", String(temperature).c_str());
+    request->send_P(200, "text/plain", String(temp).c_str());
 	});
 
 	server.on("/humidity", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send_P(200, "text/plain", String(humidity).c_str());
+    request->send_P(200, "text/plain", String(hum).c_str());
+	});  
+
+	server.on("/batteryLevel", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/plain", String(battLev).c_str());
 	});  
 
 	server.on("/deepSleep",  HTTP_POST, [] (AsyncWebServerRequest *request){
 		if (request->hasArg(sleepTimeArg))
 		{
-			sleepTime= request->arg(sleepTimeArg);			
-			
-			if(sleepTime != "0")
-				GoDeepSleep(request, sleepTime);
-			else
-				SetDeepSleepAlarm(request);	
-		}
+			String sleepTime= request->arg(sleepTimeArg);	
+			GoDeepSleep(request, sleepTime);
+		}		
 		else 
 			SetDeepSleepAlarm(request);	
 		});
@@ -87,9 +97,19 @@ void SetServerMethod(){
 void GoDeepSleep(AsyncWebServerRequest *request, String sleepTime){
 	request->send(200, "text/plain", "Server goes into deep sleep");
 	deepSleepTime = strtoll(sleepTime.c_str(),&p, 10);
+	
+	Serial.print("deepSleepTime: ");
+	Serial.println(deepSleepTime);
+	
 	shouldDeepSleep = true;
 }
 
 void SetDeepSleepAlarm(AsyncWebServerRequest *request){
 	request->send(200, "text/plain", "Server does not go into deep sleep");
+}
+
+int GetBaterryLevel()
+{
+  float voltage = (analogRead(A0) * maxVoltage) / 1024.0;
+  return ((voltage - minVoltage) / factor) * 100; 
 }
