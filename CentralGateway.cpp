c870182cabcee2714ce181e1ac878aad967db691
Tomasz Kielkowski <71585480+t-kielkowski@users.noreply.github.com>
2022-11-20 13:57:42 +0100

    Adapted for ESP32

--- CentralGateway.cpp
+++ CentralGateway.cpp
@@ -2,27 +2,23 @@
 #define TINY_GSM_RX_BUFFER 1024
 
 #include <Wire.h>
+#include <WiFi.h>
+#include <HTTPClient.h>
 #include <TinyGsmClient.h>
-#include <ESP8266WiFi.h>
-#include <ESP8266HTTPClient.h>
-#include <WiFiClient.h>
-#include <BH1750.h>
-#include "ClosedCube_HDC1080.h"
-#include "ClosedCube_SHT31D.h"
-#include <SPI.h>
-#include <Adafruit_Sensor.h>
-#include <Adafruit_BMP280.h>
+#include "Adafruit_SHT31.h"
 #include <Adafruit_ADS1X15.h>
+#include <Adafruit_BMP280.h>
+#include <BH1750.h>
 
 #define MODEM_RST 5
 #define MODEM_PWKEY 4
 #define MODEM_POWER_ON 23
 #define MODEM_TX 27
 #define MODEM_RX 26
-#define I2C_SDA 21
-#define I2C_SCL 22
-#define I2C_SDA_2 18
-#define I2C_SCL_2 19
+#define I2C_SDA_POWER 21
+#define I2C_SCL_POWER 22
+#define I2C_SDA 18
+#define I2C_SCL 19
 
 #define SerialMon Serial
 #define SerialAT Serial1
@@ -41,30 +37,29 @@ TinyGsm modem(SerialAT);
 #endif
 
 TwoWire I2CPower = TwoWire(0);
-TwoWire I2C = TwoWire(1);
-BH1750 lightMeter;
-ClosedCube_HDC1080 hdc1080;
-ClosedCube_SHT31D sht3xd;
-Adafruit_BMP280 bme;
-Adafruit_ADS1015 ads; 
 TinyGsmClient client(modem);
 
-const char apn[] = "internet";
-const char gprsUser[] = "";
-const char gprsPass[] = "";
-const char simPIN[] = "";
+Adafruit_SHT31 sht31 = Adafruit_SHT31();
+Adafruit_BMP280 bmp280;
+BH1750 lightMeter;
+Adafruit_ADS1015 ads;
 
-const char Server[] = "moj-miod.pl";
-const char CurrentTime[] = "/get-current-time.php";
-const char PostSensorData[] = "/post-sht31.php"; 
-const int Port = 80;
+const char *apn = "internet";
+const char *gprsUser = "";
+const char *gprsPass = "";
 
+const char *server = "moj-miod.pl";
+String apiKeyValue = "tPmAT5Ab3j7F9";
+const int port = 80;
+const char *currentTime = "/get-current-time.php";
+const char *postWeightSensorData = "/post-weight-data.php"; 
+const char *postGatewaySensorData = "/post-esp-data.php"; 
+
+const char *clientWeightServer = "http://192.168.4.1/weight";
 const char *clientTemperatureServer = "http://192.168.4.1/temperature";
-const char *clientHumidityServer = "http://192.168.4.1/humidity";
+const char *clientHumidityServer = "http://192.168.4.1/moisture";
 const char *clientBatteryLevel = "http://192.168.4.1/batteryLevel";
 const char *clientDeepSleepServer = "http://192.168.4.1/deepSleep";
-const char *postServerName = "http://moj-miod.pl/post-sht31.php";
-String apiKeyValue = "tPmAT5Ab3j7F9";
 
 const float maxVoltage = 3.45f;
 const float minVoltage = 2.50f;
@@ -73,75 +68,81 @@ const float factor = maxVoltage - minVoltage;
 String _noData = "NoData";
 String _currentTime = "CurrentTime:";
 
-//const char *weightSsid = "UL_1";
-char *weightSsid[] = {"UL_1", "UL_2"};
+const char * weightSsids[] = {"UL_1"};
+const int weightArraySize = (sizeof(weightSsids) / sizeof(weightSsids[0]));
 
 void setup()
 {
 	SerialMon.begin(115200);
 	SetI2C();
 	SetModemPins();
+	SynchronizeDataTransferTimeToServer();
 }
 
 void loop()
 {
-	if(ConnectToMobileInternet())
+	String httpRequestData[weightArraySize];
+
+	for (int i = 0; i < weightArraySize; i++)
 	{
-		//SynchronizeDataTransferTimeToServer();
+		auto weightSsid = weightSsids[i];
+		ConnectToScaleUsingWifi(weightSsid);
 
-		String temperature;
-		String humidity;
-		String batteryLevel;
-		int weightArraySize = (sizeof(weightSsid) / sizeof(weightSsid[0]));
-		SerialMon.print("weightArraySize: ");
-		SerialMon.println(weightArraySize);
+		auto weightId = String(weightSsid);
+		auto weight = GetSensorDataFromScale(clientWeightServer);
+		auto temperature = GetSensorDataFromScale(clientTemperatureServer);
+		auto moisture = GetSensorDataFromScale(clientHumidityServer);
+		auto batteryLevel = GetSensorDataFromScale(clientBatteryLevel);
 
-		int isSuccess[weightArraySize];	
+		DisconnectWiFi();
 
-		for (int i = 0; i <= weightArraySize; i++)
-		{
-			String weight = weightSsid[i];
-			ConnectToScaleUsingWifi(weight);
-
-			temperature = GetSensorDataFromScale(clientTemperatureServer);
-			humidity = GetSensorDataFromScale(clientHumidityServer);
-			batteryLevel = GetSensorDataFromScale(clientBatteryLevel);
-
-			if (temperature != _noData && humidity != _noData)
-			{
-				String httpRequest = CreateHttpRequestDataBasedOnScaleReadings(weight, temperature, humidity, batteryLevel);
-				SendSensorsDataToServer(httpRequest);
-				isSuccess[i] = 1;
-			}
-			else
-			{
-				SendAlertSms(weight);
-				isSuccess[i] = 0;
-			}
-		}	
-
-		PutAllScalesToSleeAndPutYourselfToSleep(isSuccess, weightArraySize);
+		if (temperature != _noData && moisture != _noData && batteryLevel != _noData && batteryLevel != _noData)
+			httpRequestData[i] = CreateHttpRequestDataBasedOnScaleReadings(weightId, weight, temperature, moisture, batteryLevel);
+		else
+			httpRequestData[i] = _noData;		
 	}
 
-	delay(5000);
+	if (ConnectToMobileInternet())
+	{
+		for (int i = 0; i < weightArraySize; i++)
+		{
+			if (httpRequestData[i] == _noData)
+				SendAlertSms(String(weightSsids[i]));
+			else
+				SendSensorsDataToServer(httpRequestData[i], postWeightSensorData);
+		}
+
+		DisconnectMobileInternet();
+	}
+
+	SendReadingsFromYourSensors();
+	PutAllScalesToSleeAndPutYourselfToSleep(httpRequestData);
+
+	delay(60000);
 }
 
-void SynchronizeDataTransferTimeToServer()
+void ConnectToScaleUsingWifi(const char * weightSsid)
 {	
-	String fullTime = GetCurrentTime();
-	int minutes = fullTime.substring(3, 4).toInt();
-	int seconds = 60 - fullTime.substring(5, 7).toInt();
-	int buffer = 10;
+	Serial.print("Connecting to ");
+	Serial.print(weightSsid);
+	Serial.print("   ");
+	
+	WiFi.begin(weightSsid);
 
-	if (minutes == 59 && seconds > buffer)
+	while (WiFi.status() != WL_CONNECTED)
 	{
-		delay(1000 * (seconds - buffer));
+		delay(500);
+		Serial.print(".");
 	}
-	else if (minutes > 50 && minutes < 59)
-	{
-		int minuteToSeconds = (60 - minutes) * 60;
-		delay(1000 * (minuteToSeconds + seconds - buffer));
-	}
+
+	Serial.print("  Connected to WiFi ");
+	Serial.println(weightSsid);
+}
+
+void DisconnectWiFi()
+{
+	WiFi.disconnect();
+	delay(100);
 }
 
 bool ConnectToMobileInternet()
@@ -152,9 +153,7 @@ bool ConnectToMobileInternet()
 	SerialMon.println("Initializing modem...");
 	modem.restart();
 
-	SerialMon.print("Connecting to Mobile Internet: ");
-	SerialMon.print(apn);
-
+	SerialMon.print("Connecting to Mobile Internet Orange");
 	if (!modem.gprsConnect(apn, gprsUser, gprsPass))
 	{
 		SerialMon.println(" fail");
@@ -167,22 +166,34 @@ bool ConnectToMobileInternet()
 	}
 }
 
-void ConnectToScaleUsingWifi(String weightSsid)
+void DisconnectMobileInternet()
 {
-	Serial.print("Connecting to ");
-	Serial.print(weightSsid);
-	Serial.print("   ");
+	modem.gprsDisconnect();
+	delay(100);
+	SerialMon.println(F("GPRS disconnected"));
+}
 
-	WiFi.begin(weightSsid);
-
-	while (WiFi.status() != WL_CONNECTED)
+void SynchronizeDataTransferTimeToServer()
+{
+	if (ConnectToMobileInternet())
 	{
-		delay(500);
-		Serial.print(".");
-	}
+		String fullTime = GetCurrentTime();
+		DisconnectMobileInternet();
 
-	Serial.print("  Connected to WiFi ");
-	Serial.println(weightSsid);
+		int minutes = fullTime.substring(3, 4).toInt();
+		int seconds = 60 - fullTime.substring(5, 7).toInt();
+		int buffer = 10;
+
+		if (minutes == 59 && seconds > buffer)
+		{
+			delay(1000 * (seconds - buffer));
+		}
+		else if (minutes > 50 && minutes < 59)
+		{
+			int minuteToSeconds = (60 - minutes) * 60;
+			delay(1000 * (minuteToSeconds + seconds - buffer));
+		}
+	}
 }
 
 String GetSensorDataFromScale(const char *clientServerName)
@@ -197,36 +208,24 @@ String GetSensorDataFromScale(const char *clientServerName)
 
 	if (httpResponseCode > 0)
 	{
-		result = http.getString();
+		 result = http.getString();
 	}
 	else
 	{
 		Serial.print("Error code: ");
-		Serial.println(httpResponseCode);
+		Serial.println(httpResponseCode);	
 	}
 
 	http.end();
 	return result;
 }
 
-String CreateHttpRequestDataBasedOnScaleReadings(String sensorName, String temperature, String humidity, String batteryLevel)
-{
-	String httpRequestData =
-		"api_key=" + apiKeyValue +
-		"&SHT31_Sensor=" + String(sensorName) +
-		"&SHT31_Temp=" + String(temperature) +
-		"&SHT31_Mois=" + String(humidity) +
-		"&SHT31_BatteryLevel=" + String(batteryLevel) + "";
-
-	return httpRequestData;
-}
-
-void SendSensorsDataToServer(String httpRequestData)
+void SendSensorsDataToServer(String httpRequestData, const char *postUrl)
 {
 	SerialMon.print("Connecting to ");
-	SerialMon.print(Server);
+	SerialMon.print(server);
 
-	if (!client.connect(Server, Port))
+	if (!client.connect(server, port))
 	{
 		SerialMon.println(" fail");
 	}
@@ -235,8 +234,8 @@ void SendSensorsDataToServer(String httpRequestData)
 		SerialMon.println(" OK");
 		SerialMon.println("Performing HTTP POST request...");
 
-		client.print(String("POST ") + PostSensorData + " HTTP/1.1\r\n");
-		client.print(String("Host: ") + Server + "\r\n");
+		client.print(String("POST ") + postUrl + " HTTP/1.1\r\n");
+		client.print(String("Host: ") + server + "\r\n");
 		client.println("Connection: close");
 		client.println("Content-Type: application/x-www-form-urlencoded");
 		client.print("Content-Length: ");
@@ -259,6 +258,56 @@ void SendSensorsDataToServer(String httpRequestData)
       client.stop();
       SerialMon.println(F("Server disconnected"));
 	}
+}
+
+void SendReadingsFromYourSensors()
+{
+	InitializeSensor();
+	String httpRequest = CreateHttpRequestDataBasedOnYourSensorsReadings();
+	SendSensorsDataToServer(httpRequest, postGatewaySensorData);
+}
+
+void PutAllScalesToSleeAndPutYourselfToSleep(String *httpRequestData)
+{
+	String fullTime = GetCurrentTime();
+
+	int minutes = 60 - fullTime.substring(3, 4).toInt();
+	int seconds = 60  - fullTime.substring(5, 7).toInt();
+
+	int buffer = 30;
+	int minutesToSeconds = (minutes) * 60; // +/- 3 hours
+	int considerNumberOfScales = weightArraySize * 15;
+
+	long long sleepTime;
+	sleepTime = (minutesToSeconds + seconds - considerNumberOfScales - buffer) * uSToSecondsFacotr;
+
+	for (int i = 0; i <= weightArraySize; i++)
+	{
+		if (httpRequestData[i] == _noData)
+		{
+			SendDeepSleepCommandToClient(weightSsids[i], sleepTime);
+			sleepTime -= 15 * uSToSecondsFacotr;
+		}
+	}
+
+	ESP.deepSleep(sleepTime);
+}
+
+void SendDeepSleepCommandToClient(const char * weightSsid, long long sleepTime)
+{
+	ConnectToScaleUsingWifi(weightSsid);
+	WiFiClient client;
+	HTTPClient httpClient;
+
+	String data = "sleepTime=" + sleepTime;
+	httpClient.begin(client, clientDeepSleepServer);
+	httpClient.addHeader("Content-Type", "application/x-www-form-urlencoded");
+	int httpResponseCode = httpClient.POST(data);
+	String content = httpClient.getString();
+	httpClient.end();
+
+	Serial.print("ANSWER POST content: ");
+	Serial.println(content);
 }
 
 String GetCurrentTime()
@@ -300,99 +349,50 @@ String GetCurrentTime()
 	}
 }
 
-void SendReadingsFromYourSensors()
+int GetSoilMoisture()
 {
-	InitializeSensor();
-	String httpRequest = CreateHttpRequestDataBasedOnYourSensorsReadings(sht3xd.readTempAndHumidity(SHT3XD_REPEATABILITY_HIGH, SHT3XD_MODE_POLLING, 50));
-	SendSensorsDataToServer(httpRequest);
-}
+  int16_t adc1;
+  adc1 = ads.readADC_SingleEnded(1);
 
-String CreateHttpRequestDataBasedOnYourSensorsReadings(SHT31D result)
-{
-	String httpRequestData =
-		"api_key=" + apiKeyValue +
-		"&GatewayId=" + gatewayId +
-		"&BH1750_Luks=" + String(lightMeter.readLightLevel()) +
-		"&BMP280_Pressure=" + String((bme.readPressure() / 100.0f)) +
-		"&BMP280_Temp=" + String(bme.readTemperature()) +
-		"&HDC1080_Temp=" + String(hdc1080.readTemperature()) +
-		"&HDC1080_Mois=" + String(hdc1080.readHumidity()) +
-		"&SHT31_Temp=" + String(result.t) +
-		"&SHT31_Mois=" + String(result.rh) +
-		"&SoilMoisture=" + String(GetSoilMoisture()) +
-		"&BatteryLevel=" + String(GetBaterryLevel()) + "";
-
-	return httpRequestData;
-}
-
-void PutAllScalesToSleeAndPutYourselfToSleep(int isSuccess[], int weightArraySize)
-{
-	String fullTime = GetCurrentTime();
-	modem.gprsDisconnect();
-	SerialMon.println(F("GPRS disconnected"));
-
-	int minutes = 60 - fullTime.substring(3, 4).toInt();
-	int seconds = 60  - fullTime.substring(5, 7).toInt();
-
-	int buffer = 30;
-	int minutesToSeconds = (minutes) * 60; // +/- 3 hours
-	int considerNumberOfScales = weightArraySize * 15;
-
-	long long sleepTime;
-	sleepTime = (minutesToSeconds + seconds - considerNumberOfScales - buffer) * uSToSecondsFacotr;
-
-	for (int i = 0; i <= weightArraySize; i++)
-	{		
-		if (isSuccess[i] == 1)
-		{
-			SendDeepSleepCommandToClient(weightSsid, sleepTime);
-			sleepTime -= 15 * uSToSecondsFacotr;
-		}
-	}
-
-	WiFi.forceSleepBegin();
-	ESP.deepSleep(sleepTime);
-}
-
-void SendDeepSleepCommandToClient(String weightSsid, long long sleepTime)
-{
-	ConnectToScaleUsingWifi(weightSsid);
-	WiFiClient client;
-	HTTPClient httpClient;
-
-	String data = "sleepTime=" + String(sleepTime);
-	httpClient.begin(client, clientDeepSleepServer);
-	httpClient.addHeader("Content-Type", "application/x-www-form-urlencoded");
-	int httpResponseCode = httpClient.POST(data);
-	String content = httpClient.getString();
-	httpClient.end();
-
-	Serial.print("ANSWER POST content: ");
-	Serial.println(content);
-}
-
-void SendAlertSms(String smsMessage){
-
-	if (!modem.sendSMS(PhoneNumber, smsMessage))
-	{
-		SerialMon.println("SMS failed to send");
-	}
+  return map(adc1, 1090.0f, 762.0f, 0, 100);
 }
 
 int GetBaterryLevel()
 {
-  int16_t adc1;
-  adc1 = ads.readADC_SingleEnded(1); 
-  
-  float voltage = (adc1 * maxVoltage) / 1121.0;
+  int16_t adc0;      
+  adc0 = ads.readADC_SingleEnded(0);
+
+  float voltage = (adc0 * maxVoltage) / 1121.0;
   return ((voltage - minVoltage) / factor) * 100; 
 }
 
-int GetSoilMoisture()
+String CreateHttpRequestDataBasedOnScaleReadings(String weightId, String weight, String temperature, String moisture, String batteryLevel)
 {
-  int16_t adc0;      
-  adc0 = ads.readADC_SingleEnded(0);  
-  return map(adc0, 1090.0f, 762.0f, 0, 100);
+	String httpRequestData =
+		"api_key=" + apiKeyValue +
+		"&WeightId=" + String(weightId) +
+		"&Weight=" + String(weight) +
+		"&Temperature=" + String(temperature) +
+		"&Moisture=" + String(moisture) +
+		"&BatteryLevel=" + String(batteryLevel) + "";
+
+	return httpRequestData;
+}
+
+String CreateHttpRequestDataBasedOnYourSensorsReadings()
+{
+	String httpRequestData =
+		"api_key=" + apiKeyValue +
+		"&GatewayId=" + "Trawniki" +
+		"&BH1750_Luks=" + String(lightMeter.readLightLevel()) +
+		"&BMP280_Pressure=" + String(bmp280.readPressure() / 100.0f) +
+		"&BMP280_Temp=" + String(bmp280.readTemperature()) +
+		"&SHT31_Temp=" + String(sht31.readTemperature()) +
+		"&SHT31_Mois=" + String(sht31.readHumidity()) +
+		"&SoilMoisture=" + String(GetSoilMoisture()) +
+		"&BatteryLevel=" + String(GetBaterryLevel()) + "";
+
+	return httpRequestData;
 }
 
 void SetModemPins()
@@ -407,9 +407,8 @@ void SetModemPins()
 
 void SetI2C()
 {
-	I2CPower.begin(I2C_SDA, I2C_SCL, 400000);
-	I2C.begin(I2C_SDA_2, I2C_SCL_2, 400000);
-	
+	I2CPower.begin(I2C_SDA_POWER, I2C_SCL_POWER, 400000);
+
 	bool isOk = setPowerBoostKeepOn(1);
 	SerialMon.println(String("IP5306 KeepOn ") + (isOk ? "OK" : "FAIL"));
 }
@@ -428,17 +427,23 @@ bool setPowerBoostKeepOn(int en)
 }
 
 void InitializeSensor()
-{	
-	Wire.begin();
+{
+	Wire.begin(I2C_SDA, I2C_SCL);
 
-    // Initialize BH1750
-    lightMeter.begin();
-    // Initialize HDC1080
-    hdc1080.begin(0x40);
-    // Initialize SHT31
-    sht3xd.begin(0x44);
-    // Initialize BMP280
-    bme.begin(0x76);
+	// Initialize BH1750
+	lightMeter.begin(BH1750::CONTINUOUS_HIGH_RES_MODE, 0x23);
+	// Initialize SHT31
+	sht31.begin(0x44);
+	// Initialize BMP280
+	bmp280.begin(0x76);
 	// Initialize ADS1X15
-    ads.begin();	
+	ads.begin(0x48);
+}
+
+void SendAlertSms(String smsMessage)
+{
+	if (!modem.sendSMS(PhoneNumber, smsMessage))
+	{
+		SerialMon.println("SMS failed to send");
+	}
 }
